33dd325b7f198989833dfaec49de5de285166682
vsfarrar <vsfarrar@ucdavis.edu>
2023-01-24 20:50:31 -0700

    trying to clear repository to retest binder

--- test_code.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-
-setwd("~/Documents/GitHub/LAK-sandbox/")
-
-#load Biology subset of Ben Koester's PLA data
-testdat <- read.csv("biol_student_record.csv")
-
-#return number of students per course catalog number, per term
-testdat %>% count(CATALOG_NBR, TERM)
